49e3bb39e8ad5fec1cdf87fcdfc9fee0cd744646
wcong <wc19920415@gmail.com>
2016-01-15 21:55:13 +0800

    ApplicationListenerTest

---
+++ src/main/java/org/wcong/test/ApplicationListenerTest.java
@@ -0,0 +1,45 @@
+package org.wcong.test;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author wcong<wc19920415@gmail.com>
+ * @since 16/1/15
+ */
+@Configuration
+public class ApplicationListenerTest {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+		annotationConfigApplicationContext.register(ApplicationListenerTest.class);
+		annotationConfigApplicationContext.refresh();
+		PrintSomething printSomething = annotationConfigApplicationContext.getBean(PrintSomething.class);
+		printSomething.print();
+	}
+
+	public interface PrintSomething {
+		void print();
+	}
+
+	@Service
+	public static class PrintHello implements PrintSomething {
+
+		public void print() {
+			System.out.println("hello");
+		}
+	}
+
+	@Component
+	public static class ApplicationListenerComponent implements ApplicationListener {
+
+		public void onApplicationEvent(ApplicationEvent event) {
+			System.out.println(event);
+		}
+	}
+
+}
