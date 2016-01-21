056b3af80a3ca915f4f47002a584af67b68b2977
wcong <wc19920415@gmail.com>
2016-01-21 16:22:39 +0800

    first embed Tomcat

--- src/main/java/org/wcong/test/tomcat/TomcatListenerTest.java
+++ src/main/java/org/wcong/test/tomcat/TomcatListenerTest.java
@@ -1,10 +1,16 @@
 package org.wcong.test.tomcat;
 
 import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.core.StandardWrapper;
 import org.apache.catalina.startup.Tomcat;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * StandardContext.applicationLifecycleListenersObjects
@@ -20,8 +26,19 @@ public class TomcatListenerTest {
 		standardContext.setName("test");
 		standardContext.setPath("");
 		standardContext.setDocBase("");
-		standardContext.addWelcomeFile("welcome.txt");
+
+		// important Fix startup sequence - required if you don't use web.xml.
+		standardContext.addLifecycleListener(new Tomcat.FixContextListener());
+
 		standardContext.addApplicationLifecycleListener(new ServletContextListenerTest());
+
+		StandardWrapper standardWrapper = new StandardWrapper();
+		standardWrapper.setName("standardWrapper");
+		standardWrapper.setServletClass(ServletTest.class.getName());
+
+		standardContext.addChild(standardWrapper);
+		standardContext.addServletMapping("/","standardWrapper");
+		tomcat.getService();
 		tomcat.getHost().setAppBase(".");
 		tomcat.getHost().addChild(standardContext);
 		tomcat.start();
@@ -31,11 +48,20 @@ public class TomcatListenerTest {
 	public static class ServletContextListenerTest implements ServletContextListener {
 
 		public void contextInitialized(ServletContextEvent sce) {
-			System.out.println("contextInitialized" + sce);
+			System.out.println("contextInitialized " + sce);
 		}
 
 		public void contextDestroyed(ServletContextEvent sce) {
-			System.out.println("contextDestroyed" + sce);
+			System.out.println("contextDestroyed " + sce);
 		}
+	}
+
+	public static class ServletTest extends HttpServlet {
+		@Override
+		public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			resp.getWriter().write("test");
+			resp.flushBuffer();
+		}
+
 	}
 }
