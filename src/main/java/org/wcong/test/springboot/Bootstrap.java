ce08ff7e96202ba2b003ccd09e9ed64588ee75ce
wcong <wc19920415@gmail.com>
2016-05-26 21:06:50 +0800

    add spring boot

---
+++ src/main/java/org/wcong/test/springboot/Bootstrap.java
@@ -0,0 +1,36 @@
+package org.wcong.test.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by hzwangcong on 2016/5/26.
+ */
+@Configuration
+@EnableAutoConfiguration
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(Bootstrap.class);
+        springApplication.run(args);
+    }
+
+    @Controller
+    public static class MyController {
+        @RequestMapping
+        @ResponseBody
+        public Map index() {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("hello", "world");
+            return map;
+        }
+    }
+
+}
