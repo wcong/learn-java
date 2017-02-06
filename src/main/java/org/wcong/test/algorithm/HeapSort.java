ed1cc7045941c9c58ea58193b5f4da4c69dde9ca
wcong <wc19920415@gmail.com>
2017-02-06 14:27:06 +0800

    add heap sort

---
+++ src/main/java/org/wcong/test/algorithm/HeapSort.java
@@ -0,0 +1,33 @@
+package org.wcong.test.algorithm;
+
+/**
+ * Created by hzwangcong on 2017/2/5.
+ */
+public class HeapSort {
+
+    public static void main(String[] args) {
+
+    }
+
+    static void maxHeapify(int[] array, int top) {
+        if (top >= array.length) {
+            return;
+        }
+        int left = (top + 1) * 2 - 1;
+        int right = (top + 1) * 2;
+        int large = top;
+        if (left < array.length && array[left] > array[large]) {
+            large = left;
+        }
+        if (right < array.length && array[right] > array[large]) {
+            large = right;
+        }
+        if (large != top) {
+            int temp = array[top];
+            array[top] = array[large];
+            array[large] = temp;
+            maxHeapify(array, large);
+        }
+    }
+
+}
