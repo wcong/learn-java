ff721926d78d7be9aba4aa8659fe613822c024ad
wcong <wc19920415@gmail.com>
2017-03-29 11:05:35 +0800

    add UglyNumber

---
+++ src/main/java/org/wcong/test/algorithm/jzoffer/UglyNumber.java
@@ -0,0 +1,39 @@
+package org.wcong.test.algorithm.jzoffer;
+
+import org.springframework.util.Assert;
+
+/**
+ * test for number
+ * ugly number only contains 2,3,4 for multiply count target number of ugly number
+ * Created by wcong on 2017/3/29.
+ */
+public class UglyNumber {
+
+    public static void main(String[] args) {
+        Assert.isTrue(nThUglyNumber(9) == 12);
+        Assert.isTrue(nThUglyNumber(10) == 15);
+    }
+
+    public static int nThUglyNumber(int n) {
+        int[] ugly = new int[n + 1];
+        ugly[0] = 1;
+        int index2 = 0;
+        int index3 = 0;
+        int index5 = 0;
+        for (int i = 1; i <= n; i++) {
+            int min = Math.min(Math.min(ugly[index2] * 2, ugly[index3] * 3), ugly[index5] * 5);
+            ugly[i] = min;
+            while (ugly[index2] * 2 <= min) {
+                index2 += 1;
+            }
+            while (ugly[index3] * 3 <= min) {
+                index3 += 1;
+            }
+            while (ugly[index5] * 5 <= min) {
+                index5 += 1;
+            }
+        }
+        return ugly[n];
+    }
+
+}
