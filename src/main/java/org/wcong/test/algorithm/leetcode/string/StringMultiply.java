5143783fcc6407be298bbec4390ec8d5763df226
wcong <wc19920415@gmail.com>
2017-04-01 18:24:38 +0800

    add StringMultiply

---
+++ src/main/java/org/wcong/test/algorithm/leetcode/string/StringMultiply.java
@@ -0,0 +1,17 @@
+package org.wcong.test.algorithm.leetcode.string;
+
+/**
+ * give two string of integer, calculate the product of them
+ * Created by wcong on 2017/4/1.
+ */
+public class StringMultiply {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String multiply(String a, String b) {
+        return null;
+    }
+
+}
